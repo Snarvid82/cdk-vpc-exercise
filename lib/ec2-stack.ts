4249b6f74bfc1145f9d3637e75ebeed7cd5c4a71
Snarvid82 <arvid.vik@gmail.com>
2025-11-01 10:58:39 +0100

    EC2 instances created

---
+++ lib/ec2-stack.ts
@@ -0,0 +1,49 @@
+import * as cdk from 'aws-cdk-lib/core';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+
+// Props
+
+interface EC2StackProps extends cdk.StackProps {
+    vpc: ec2.Vpc;
+}
+
+export class EC2Stack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props: EC2StackProps) {
+        super(scope, id, props);
+
+    // EC2 instance 1 in AZ 1
+
+    const instance1 = new ec2.Instance(this, 'MyPrivateEC2-AZ1', {
+        vpc: props.vpc,
+        vpcSubnets: {
+            subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+            availabilityZones: [props.vpc.availabilityZones[0]]
+        },
+        machineImage: new ec2.AmazonLinuxImage({
+            generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
+        }),
+        instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO)
+    })
+
+        cdk.Tags.of(instance1).add('Name', 'MyPrivateEC2-AZ1' )
+
+    // EC2 instance 2 in AZ 2
+
+    const instance2 = new ec2.Instance(this, 'MyPrivateEC2-AZ2', {
+        vpc: props.vpc,
+        vpcSubnets: {
+            subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+            availabilityZones: [props.vpc.availabilityZones[1]]
+        },
+        machineImage: new ec2.AmazonLinuxImage({
+            generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
+        }),
+        instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO)
+    })
+
+        cdk.Tags.of(instance2).add('Name', 'MyPrivateEC2-AZ2' )
+
+    }
+}
+
--- lib/vpc-cdk-project-stack.ts
+++ lib/vpc-cdk-project-stack.ts
@@ -3,10 +3,12 @@ import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2'
 
 export class VpcCdkProjectStack extends cdk.Stack {
+  public readonly vpc: ec2.Vpc
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'MyVpc', {
+    this.vpc = new ec2.Vpc(this, 'MyVpc', {
       maxAzs: 2,
       subnetConfiguration: [
         {
@@ -28,7 +30,7 @@ export class VpcCdkProjectStack extends cdk.Stack {
     })
 
     new cdk.CfnOutput(this, 'VpcId', {
-      value: vpc.vpcId,
+      value: this.vpc.vpcId,
       description: 'VPC ID'
     })
   }
